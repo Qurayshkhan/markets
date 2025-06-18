1778963606d7e768c297d4eead6de53d55c29929
Qurayshkhan <96570166+Qurayshkhan@users.noreply.github.com>
2025-06-18 12:09:49 +0500

    first commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("CDP_DB_HOST", "localhost");
+define("CDP_DB_NAME", "deprixapro_database");
+define("CDP_DB_USER", "hassankhan");
+define("CDP_DB_PASS", "Hassan*123");
+define('CDP_APP_URL', 'http://localhost/my-projects/marketz/marketz/marketz/');
+define('CDP_APP_MODE_DEMO', false);
+?>
